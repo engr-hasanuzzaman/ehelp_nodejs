3a64128294874980ff1dba81eb5a227617654299
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2017-12-15 00:46:40 +0600

    add event emitter practice file

---
+++ event_emitter.js
@@ -0,0 +1,25 @@
+const EventEmitter = require('events')
+
+class Event extends EventEmitter {}
+
+const job = new Event();
+job.on('done', (timeDone) => {
+    console.log('Job done time is ' + timeDone);
+})
+
+job.emit('done', new Date());
+job.removeAllListeners()
+
+knock = new Event();
+// once ensure that knock will be fire only one time
+knock.once('knock', ()=>{
+  console.log('this is first knock');  
+});
+
+// knock.on('knock', ()=> {
+//    console.log('this is second knock')
+// })
+
+knock.emit('knock')
+// knock.removeAllListeners()
+knock.emit('knock')

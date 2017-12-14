a9e8c62250e5372fd9bd31c46a7d1e92ddcd0247
engr_hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2017-12-14 18:48:52 +0600

    create path testing file

---
+++ test_path.js
@@ -0,0 +1,6 @@
+const path = require('path')
+
+const firstPath = path.join('first', 'seconde', '3rd')
+const secondPath = path.join(__dirname, 'first', 'second', '3rd')
+console.log('first log is ' + firstPath);
+console.log('last log is ' + secondPath);

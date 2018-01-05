41a9fd8581b415bd94024b1969c89d86335c2e94
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2018-01-05 22:56:32 +0600

    create rest api for post comment

--- rest-blog/routes/comments.js
+++ rest-blog/routes/comments.js
@@ -1,5 +1,41 @@
-module.exports = function(app){
+const _ = require("lodash")
+
+module.exports = function(app, store){
+    // fetch all comments under post
     app.get("/posts/:postId/comments", (req, res) => {
-        res.json({success: true })
+        let post = store.posts[req.params.postId]
+        res.json(post.comments)
+    })
+
+    // fetch comment by passing id under post
+    app.get("/posts/:postId/comments/:commentId", (req, res) => {
+        let post = store.posts[req.params.postId]
+        res.json(post.comments[req.params.commentId])
+    })
+
+    // fetch all post under post
+    app.post("/posts/:postId/comments", (req, res) => {
+        let post = store.posts[req.params.postId]
+        let comment = {
+            text: req.body.text
+        }
+        post.comments.push(comment)
+        res.sendStatus(201)
+    })
+
+    // fetch all post under post
+    app.put("/posts/:postId/comments/:commentId", (req, res) => {
+        let post = store.posts[req.params.postId]
+        let comment = { text: req.body.text }
+        Object.assign(post.comments[req.params.commentId], _.pickBy(comment))
+        res.sendStatus(204)
+    })
+
+
+    // fetch all post under post
+    app.delete("/posts/:postId/comments/:commentId", (req, res) => {
+        let post = store.posts[req.params.postId]
+        post.comments.splice(req.params.commentId, 1)
+        res.sendStatus(204)
     })
 }
